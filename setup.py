ec8fb689cd3b0bc43f72b01d0cbb1ff844f2ab45
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 17:31:20 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-label-slider',
@@ -10,6 +18,8 @@ setup(
     description='PyQt QSlider with QLabel(QLabel is on the left side, QSlider is on the right side, horizontal direction only) '
                 'QLabel\'s value synchronizes with QSlider\'s value.',
     url='https://github.com/yjg30737/pyqt-label-slider.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
